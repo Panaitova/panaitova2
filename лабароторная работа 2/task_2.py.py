37e55568b5aac57f1e8d08f44df50413c9ceae1c
Panaitova <159958176+Panaitova@users.noreply.github.com>
2024-10-30 13:40:21 +0300

    Update task_2.py.py

--- лабароторная работа 2/task_2.py.py
+++ лабароторная работа 2/task_2.py.py
@@ -1,21 +1,23 @@
+import math
+
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
-total_needed_capital = 0 # Общая подушка безопастности
+total_needed_capital = 0  # Общая подушка безопастности
 
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
-#Считаем расходы за каждый месяц
-for month in range(months):
+# Считаем расходы за каждый месяц
+for _ in range(months):
     # Рассчитываем нехватку средств текущего месяца
     if spend > salary:
         deficit = spend - salary
-        total_needed_capital += deficit # Суммируем нехватку в общую подушку безопастности
+        total_needed_capital += deficit  # Суммируем нехватку в общую подушку безопастности
 
     # Увеличиваем расходы для следующего месяца
     spend *= (1 + increase)
 
 # Округляем до целого числа
-total_needed_capital = round(total_needed_capital)
+total_needed_capital = math.trunc(total_needed_capital)
 
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов: {total_needed_capital}")
