00ca8a9aa7bd9791f80e4372413a145c5043a909
Panaitova <159958176+Panaitova@users.noreply.github.com>
2024-11-08 17:40:54 +0300

    Update task_2.py

--- лабораторная работа 3/task_2.py
+++ лабораторная работа 3/task_2.py
@@ -1,17 +1,19 @@
 # TODO Напишите функцию find_common_participants
-def find_common_participants(group1, group2, separator="|"):
+def find_common_participants(group1, group2, separator=","):
     participants1 = set(group1.split(separator))
     participants2 = set(group2.split(separator))
-    return list(participants1.intersection(participants2))
+    common_participant = list(participants1.intersection(participants2))
+    common_participant.sort()
+    return common_participant
 
 
-participants_first_group = "Иванов|Петров|Сидоров"
-participants_second_group = "Петров|Сидоров|Смирнов"
+participants_first_group = "Иванов,Петров,Сидоров"
+participants_second_group = "Петров,Сидоров,Смирнов"
 common_participants = find_common_participants(participants_first_group, participants_second_group)
 print(f"Общие участники: {common_participants}")
 
-# TODO Проверьте работу функции с разделителем отличным от запятой
-participants_first_group = "Иванов,Петров,Сидоров"
-participants_second_group = "Петров,Сидоров,Смирнов"
-common_participants = find_common_participants(participants_first_group, participants_second_group, separator=",")
+# TODO Провеьте работу функции с разделителем отличным от запятой
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+common_participants = find_common_participants(participants_first_group, participants_second_group, separator="|")
 print(f"Общие участники: {common_participants}")
